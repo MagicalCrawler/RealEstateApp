880d0f5daed7b08b175cd76750f78e70676304f7
Ahmad Amiri <amiriahmad72@gmail.com>
2024-11-07 23:04:09 +0330

    feat: add database connection utility

--- db/database.go
+++ db/database.go
@@ -1,1 +1,25 @@
 package db
+
+import (
+	"fmt"
+
+	"github.com/MagicalCrawler/RealEstateApp/utils"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewConnection() *gorm.DB {
+	host := utils.GetConfig("POSTGRES_HOST")
+	user := utils.GetConfig("POSTGRES_USER")
+	password := utils.GetConfig("POSTGRES_PASSWORD")
+	dbname := utils.GetConfig("POSTGRES_DB_NAME")
+	port := utils.GetConfig("POSTGRES_PORT")
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tehran", host, user, password, dbname, port)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+		panic("Error connecting to database")
+	}
+
+	return db
+}
