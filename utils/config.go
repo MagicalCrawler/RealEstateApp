14a4b9041b36c434c0fd5ecf7f1d3d7f0db2b3b6
seyedah <seyedah2000@gmail.com>
2024-11-09 23:39:46 +0330

    correction .config

--- utils/config.go
+++ utils/config.go
@@ -12,9 +12,9 @@ func GetConfig(key string) string {
 }
 
 func LoadEnvFile() {
-	err := godotenv.Load(".env.example")
+	err := godotenv.Load(".env")
 	if err != nil {
 		fmt.Println(err)
-		panic("Error loading .env.example file")
+		panic("Error loading .env file")
 	}
 }
