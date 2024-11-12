f303ef73cd4980019fdc58fffa8803d242c89cc4
ali14taghavi <ali14taghavi@gmail.com>
2024-11-12 21:11:02 +0330

    search on post history function added to filter repository

--- go.mod
+++ go.mod
@@ -2,13 +2,24 @@ module github.com/MagicalCrawler/RealEstateApp
 
 go 1.23.2
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/stretchr/testify v1.8.1
+	gorm.io/driver/sqlite v1.5.6
+)
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.13.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
 
+require (
 	github.com/PuerkitoBio/goquery v1.10.0 // direct
 	github.com/andybalholm/cascadia v1.3.2 // indirect
-	golang.org/x/net v0.29.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.1 // indirect
@@ -16,8 +27,9 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	golang.org/x/crypto v0.28.0 // indirect
-	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/text v0.19.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sync v0.9.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
 	gorm.io/driver/postgres v1.5.9 // direct
 	gorm.io/gorm v1.25.12 // direct
 )
--- models/filter.go
+++ models/filter.go
@@ -2,29 +2,30 @@ package models
 
 import (
 	"time"
+
+	"gorm.io/gorm"
 )
-import "gorm.io/gorm"
 
 type FilterItem struct {
-	ID               uint       `gorm:"primaryKey" json:"id"`
-	PriceMin         float64    `json:"price_min"`
-	PriceMax         float64    `json:"price_max"`
-	City             string     `json:"city"`
-	Neighborhood     string     `json:"neighborhood"`
-	AreaMin          int        `json:"area_min"`
-	AreaMax          int        `json:"area_max"`
-	BedroomsMin      int        `json:"bedrooms_min"`
-	BedroomsMax      int        `json:"bedrooms_max"`
-	Category         string     `json:"category"` // rent, buy, mortgage
-	AgeMin           int        `json:"age_min"`
-	AgeMax           int        `json:"age_max"`
-	PropertyType     string     `json:"property_type"` // apartment, villa
-	FloorMin         int        `json:"floor_min"`
-	FloorMax         int        `json:"floor_max"`
-	HasStorage       bool       `json:"has_storage"`
-	HasElevator      bool       `json:"has_elevator"`
-	CreatedDateStart time.Time  `json:"created_date_start"`
-	CreatedDateEnd   time.Time  `json:"created_date_end"`
+	ID               uint        `gorm:"primaryKey" json:"id"`
+	PriceMin         float64     `json:"price_min"`
+	PriceMax         float64     `json:"price_max"`
+	City             string      `json:"city"`
+	Neighborhood     string      `json:"neighborhood"`
+	AreaMin          int         `json:"area_min"`
+	AreaMax          int         `json:"area_max"`
+	BedroomsMin      int         `json:"bedrooms_min"`
+	BedroomsMax      int         `json:"bedrooms_max"`
+	Category         string      `json:"category"` // rent, buy, mortgage
+	AgeMin           int         `json:"age_min"`
+	AgeMax           int         `json:"age_max"`
+	PropertyType     string      `json:"property_type"` // apartment, villa
+	FloorMin         int         `json:"floor_min"`
+	FloorMax         int         `json:"floor_max"`
+	HasStorage       bool        `json:"has_storage"`
+	HasElevator      bool        `json:"has_elevator"`
+	CreatedDateStart time.Time   `json:"created_date_start"`
+	CreatedDateEnd   time.Time   `json:"created_date_end"`
 	WatchLists       []WatchList `gorm:"foreignKey:FilterItemID"` // Optional, for reverse lookup
 }
 
@@ -75,4 +76,72 @@ func GetAllFilterItems(db *gorm.DB) ([]FilterItem, error) {
 		return nil, err
 	}
 	return filterItems, nil
+}
+
+func SearchPostHistory(db *gorm.DB, filter FilterItem) ([]PostHistory, error) {
+	var posts []PostHistory
+
+	query := db.Model(&PostHistory{})
+
+	// Apply filters based on FilterItem fields
+	if filter.PriceMin > 0 {
+		query = query.Where("price >= ?", filter.PriceMin)
+	}
+	if filter.PriceMax > 0 {
+		query = query.Where("price <= ?", filter.PriceMax)
+	}
+	if filter.City != "" {
+		query = query.Where("city = ?", filter.City)
+	}
+	if filter.Neighborhood != "" {
+		query = query.Where("neighbor = ?", filter.Neighborhood)
+	}
+	if filter.AreaMin > 0 {
+		query = query.Where("area >= ?", filter.AreaMin)
+	}
+	if filter.AreaMax > 0 {
+		query = query.Where("area <= ?", filter.AreaMax)
+	}
+	if filter.BedroomsMin > 0 {
+		query = query.Where("bedroom_num >= ?", filter.BedroomsMin)
+	}
+	if filter.BedroomsMax > 0 {
+		query = query.Where("bedroom_num <= ?", filter.BedroomsMax)
+	}
+	if filter.Category != "" {
+		query = query.Where("buy_mode = ?", filter.Category)
+	}
+	if filter.AgeMin > 0 {
+		query = query.Where("age >= ?", filter.AgeMin)
+	}
+	if filter.AgeMax > 0 {
+		query = query.Where("age <= ?", filter.AgeMax)
+	}
+	if filter.PropertyType != "" {
+		query = query.Where("building = ?", filter.PropertyType)
+	}
+	if filter.FloorMin > 0 {
+		query = query.Where("floors_num >= ?", filter.FloorMin)
+	}
+	if filter.FloorMax > 0 {
+		query = query.Where("floors_num <= ?", filter.FloorMax)
+	}
+	if filter.HasStorage {
+		query = query.Where("has_ware_house = ?", filter.HasStorage)
+	}
+	if filter.HasElevator {
+		query = query.Where("has_elevator = ?", filter.HasElevator)
+	}
+	if !filter.CreatedDateStart.IsZero() {
+		query = query.Where("created_at >= ?", filter.CreatedDateStart)
+	}
+	if !filter.CreatedDateEnd.IsZero() {
+		query = query.Where("created_at <= ?", filter.CreatedDateEnd)
+	}
+
+	// Execute query
+	if err := query.Find(&posts).Error; err != nil {
+		return nil, err
+	}
+	return posts, nil
 }
