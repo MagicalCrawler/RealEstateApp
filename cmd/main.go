f5c388011c9ae8bb59a0676de8f5c4f9d846bc08
RaziyeNikookolah <r.nikookolah@gmail.com>
2024-11-12 14:48:05 +0330

    chore: del unusable packages and lines remained

--- cmd/main.go
+++ cmd/main.go
@@ -1,36 +1,15 @@
 package main
 
 import (
-	"log"
+	telegram_client "github.com/MagicalCrawler/RealEstateApp/cmd/client"
 	"github.com/MagicalCrawler/RealEstateApp/db"
 	"github.com/MagicalCrawler/RealEstateApp/utils"
-	tgEvent "github.com/MagicalCrawler/RealEstateApp/cmd/events/telegram_event"
-	tgClient "github.com/MagicalCrawler/RealEstateApp/cmd/clients/telegram"
-	event_consumer "github.com/MagicalCrawler/RealEstateApp/cmd/consumer"
-)
-
-const (
-	tgBotHost   = "api.telegram.org"
-	batchSize   = 100
 )
 
 func main() {
 	utils.LoadEnvFile()
 	db.NewConnection()
 
-	token := utils.GetConfig("TELEGRAM_TOKEN")
-	log.Println("Bot Token:", token)
-	eventsProcessor := tgEvent.New(
-		tgClient.New(tgBotHost, token),
-	)
+	telegram_client.Run()
 
-	log.Print("service started")
-
-	consumer := event_consumer.New(eventsProcessor, eventsProcessor, batchSize)
-	
-
-	if err := consumer.Start(); err != nil {
-		log.Fatal("service is stopped", err)
-	}
 }
-
